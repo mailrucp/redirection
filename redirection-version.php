854d76f4dec527ee09e01bfe0b0e4e2738ab9647
John <johng75@gmail.com>
2024-08-10 11:29:37 +0100

    Updates for 5.5 (#3898)
    * Remove local locales
    
    * Fix search where agent is empty
    
    Otherwise it matches everything
    
    * Fix multiple 404 detect
    
    * Prepare for 5.5
    
    * 5.5.0
    
    * Fix permalink migration sanitizing %d from %day%
    
    * Update build scripts

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '5.4.2' );
-define( 'REDIRECTION_BUILD', '2dfac6cbb88915145528d6945ba453dd' );
-define( 'REDIRECTION_MIN_WP', '5.7' );
+define( 'REDIRECTION_VERSION', '5.5.0' );
+define( 'REDIRECTION_MIN_WP', '6.4' );
+define( 'REDIRECTION_BUILD', 'fe0a39d2435bdc89725a505d988b3ee2' );
